137dcebbc342e04a4ca0fe561c78b6fb75ed8203
Igor França ∴ <125696858+IgorSFranca@users.noreply.github.com>
2023-08-10 10:43:03 -0400

    Menu de opções
    Configuração do menu de opções

--- task_list.c
+++ task_list.c
@@ -1,6 +1,6 @@
 /*
  * Nome do arquivo: task_list.c
- * Descrição: Lista de Tarefas CLI: Crie uma aplicação de linha de comando (CLI) para gerenciar uma lista de tarefas. 
+ * Descrição: Lista de Tarefas: Uma aplicação de linha de comando (CLI) para gerenciar uma lista de tarefas. 
  * Os usuários podem adicionar, remover e listar tarefas.
  * Autor: Igor França
  * Data de criação: 10 de agosto de 2023
@@ -11,10 +11,21 @@
 #include <string.h>
 #include <ctype.h>
 
-void cabecalho();
+void cabecalho(), opcoes_princiais(), opcoes_secundarias();
 
 int main(){
+    int opcao;
+
     cabecalho();
+    opcoes_princiais();
+    do{
+        printf("Informe a opcao desejada: ");
+        scanf(" %i", &opcao);
+        if (opcao == 0)
+            opcoes_secundarias();
+        if (opcao == 8)
+            opcoes_princiais();
+    } while (opcao < 0 || opcao > 8 || opcao == 0 || opcao == 8);
 
     system ("pause");
     return 0;
@@ -25,5 +36,26 @@ void cabecalho(){
     printf("|                 ~#~                 |\n");
     printf("|              TASK LIST              |\n");
     printf("|                 ~#~                 |\n");
-    printf("---------------------------------------\n\n");
+    printf("---------------------------------------\n");
+}
+
+void opcoes_princiais(){
+    printf("---------------------------------------\n");
+    printf("|           Opcoes principais         |\n");
+    printf("| [1] Visualizar lista de tarefas     |\n");
+    printf("| [2] Criar tarefa                    |\n");
+    printf("| [3] Editar tarefa                   |\n");
+    printf("| [4] Excluir tarefa                  |\n");
+    printf("| [5] Concluir tarefa                 |\n");
+    printf("| [0] Mais opcoes                     |\n");
+    printf("---------------------------------------\n");
+}
+
+void opcoes_secundarias(){
+    printf("---------------------------------------\n");
+    printf("|         ~  Outras Opcoes  ~         |\n");
+    printf("| [6] Para limpar o visor             |\n");
+    printf("| [7] Para encerrar a aplicacao       |\n");
+    printf("| [8] Menu anterior                   |\n");
+    printf("---------------------------------------\n");
 }
