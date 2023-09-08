a8db24e25208bdb23b619a08b03a5581a8365fdc
Igor França ∴ <125696858+IgorSFranca@users.noreply.github.com>
2023-09-08 13:23:41 -0400

    Create task_list.c

---
+++ task_list.c
@@ -0,0 +1,23 @@
+/*
+ * Nome do arquivo: task_list.c
+ * Descrição: Aplicação de lista de tarefas, onde o usuário consegue inserir, excluir, editar, visualização e concluir tarefas.
+ * Autor: Igor França
+ * Data de criação: 08 de setembro de 2023. 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct tarefas{
+    char tarefa[50];
+    int situacao;
+};
+
+typedef struct tarefas tarefas;
+
+int main (){
+
+    
+    return 0;
+}
